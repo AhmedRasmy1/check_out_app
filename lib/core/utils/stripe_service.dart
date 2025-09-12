11634dbc11f11034b90ef6b54431bf075277a2b0
Ahmed Hisham <ahmedhishamrasmy1@gmail.com>
2025-09-12 11:53:38 +0300

    Add initPaymentSheet method to StripeService for payment sheet initialization

--- lib/core/utils/stripe_service.dart
+++ lib/core/utils/stripe_service.dart
@@ -2,6 +2,7 @@ import 'package:check_out_app/core/utils/api_keys.dart';
 import 'package:check_out_app/core/utils/api_service.dart';
 import 'package:check_out_app/features/checkout/data/models/payment_intent_input_model.dart';
 import 'package:check_out_app/features/checkout/data/models/payment_intent_model/payment_intent_model.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
 
 class StripeService {
   final ApiService apiService;
@@ -18,5 +19,14 @@ class StripeService {
 
     var paymentIntentModel = PaymentIntentModel.fromJson(response.data);
     return paymentIntentModel;
+  }
+
+  Future initPaymentSheet({required String clientSecret}) async {
+    Stripe.instance.initPaymentSheet(
+      paymentSheetParameters: SetupPaymentSheetParameters(
+        paymentIntentClientSecret: clientSecret,
+        merchantDisplayName: 'Ahmed Hisham',
+      ),
+    );
   }
 }
