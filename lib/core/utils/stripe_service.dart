accabb11ea905b501dd18ad347a987d96a0dc5ed
Ahmed Hisham <ahmedhishamrasmy1@gmail.com>
2025-09-12 11:50:37 +0300

    Add api_keys.dart to .gitignore and implement StripeService for payment intent creation

--- lib/core/utils/stripe_service.dart
+++ lib/core/utils/stripe_service.dart
@@ -1,8 +1,22 @@
+import 'package:check_out_app/core/utils/api_keys.dart';
+import 'package:check_out_app/core/utils/api_service.dart';
 import 'package:check_out_app/features/checkout/data/models/payment_intent_input_model.dart';
 import 'package:check_out_app/features/checkout/data/models/payment_intent_model/payment_intent_model.dart';
 
 class StripeService {
-  // PaymentIntentModel createPaymentIntent(
-  //   PaymentIntentInputModel paymentIntentInputModel,
-  // ) {}
+  final ApiService apiService;
+
+  StripeService({required this.apiService});
+  Future<PaymentIntentModel> createPaymentIntent(
+    PaymentIntentInputModel paymentIntentInputModel,
+  ) async {
+    final response = await apiService.post(
+      body: paymentIntentInputModel.toMap(),
+      url: 'https://api.stripe.com/v1/payment_intents',
+      token: ApiKeys.secretKey,
+    );
+
+    var paymentIntentModel = PaymentIntentModel.fromJson(response.data);
+    return paymentIntentModel;
+  }
 }
