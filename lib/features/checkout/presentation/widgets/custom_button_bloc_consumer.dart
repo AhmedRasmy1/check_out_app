adf52a1950b57d4285edfd3365828ebf7c79c50b
Ahmed Hisham <ahmedhishamrasmy1@gmail.com>
2025-09-14 12:13:09 +0300

    Implement PayPal payment option in CustomButtonBlocConsumer; add AmountModel and Details classes for payment details handling.

--- lib/features/checkout/presentation/widgets/custom_button_bloc_consumer.dart
+++ lib/features/checkout/presentation/widgets/custom_button_bloc_consumer.dart
@@ -4,6 +4,7 @@ import 'package:check_out_app/features/checkout/presentation/views/thank_you_vie
 import 'package:check_out_app/features/checkout/presentation/widgets/custom_button_for_payment.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_paypal_payment/flutter_paypal_payment.dart';
 
 class CustomButtonBlocConsumer extends StatelessWidget {
   const CustomButtonBlocConsumer({
@@ -42,36 +43,82 @@ class CustomButtonBlocConsumer extends StatelessWidget {
           width: width * 0.9,
           height: height * 0.090,
           onPressed: () {
-            PaymentIntentInputModel paymentIntentInputModel =
-                PaymentIntentInputModel(
-                  amount: '100',
-                  currency: 'USD',
-                  customerId: 'cus_T2gAyLuJuYCYFi',
-                );
-            BlocProvider.of<PaymentCubit>(
-              context,
-            ).createPayment(paymentIntentInputModel: paymentIntentInputModel);
-            // if (selectedIndex == 0) {
-            //   Navigator.push(
-            //     context,
-            //     MaterialPageRoute(
-            //       builder: (_) => CustomCreditCardWidget(
-            //         formKey: formKey,
-            //         autoValidateMode: autoValidateMode,
-            //       ),
-            //     ),
-            //   );
-            // } else if (selectedIndex == 1) {
-            //   Navigator.push(
-            //     context,
-            //     MaterialPageRoute(builder: (_) => const PaypalPage()),
-            //   );
-            // } else if (selectedIndex == 2) {
-            //   Navigator.push(
-            //     context,
-            //     MaterialPageRoute(builder: (_) => const ApplePayPage()),
-            //   );
-            // }
+            if (selectedIndex == 0) {
+              // Stripe
+              PaymentIntentInputModel paymentIntentInputModel =
+                  PaymentIntentInputModel(
+                    amount: '100',
+                    currency: 'USD',
+                    customerId: 'cus_T2gAyLuJuYCYFi',
+                  );
+              BlocProvider.of<PaymentCubit>(
+                context,
+              ).createPayment(paymentIntentInputModel: paymentIntentInputModel);
+            } else if (selectedIndex == 1) {
+              // Paypal
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (BuildContext context) => PaypalCheckoutView(
+                    sandboxMode: true,
+                    clientId: "",
+                    secretKey: "",
+                    transactions: const [
+                      {
+                        "amount": {
+                          "total": '70',
+                          "currency": "USD",
+                          "details": {
+                            "subtotal": '70',
+                            "shipping": '0',
+                            "shipping_discount": 0,
+                          },
+                        },
+                        "description": "The payment transaction description.",
+                        "item_list": {
+                          "items": [
+                            {
+                              "name": "Apple",
+                              "quantity": 4,
+                              "price": '5',
+                              "currency": "USD",
+                            },
+                            {
+                              "name": "Pineapple",
+                              "quantity": 5,
+                              "price": '10',
+                              "currency": "USD",
+                            },
+                          ],
+                        },
+                      },
+                    ],
+                    note: "Contact us for any questions on your order.",
+                    onSuccess: (Map params) async {
+                      print("onSuccess: $params");
+                    },
+                    onError: (error) {
+                      print("onError: $error");
+                      Navigator.pop(context);
+                    },
+                    onCancel: () {
+                      print('cancelled:');
+                    },
+                  ),
+                ),
+              );
+            } else if (selectedIndex == 2) {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (_) => Scaffold(
+                    appBar: AppBar(title: const Text('قريباً')),
+                    body: const Center(
+                      child: Text('سيتم إضافة طريقة الدفع قريباً'),
+                    ),
+                  ),
+                ),
+              );
+            }
           },
           text: 'Choose Payment Method',
         );
