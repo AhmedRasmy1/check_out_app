f093f1674c98cfd3d5383ee12514a6e0a00b4425
Ahmed Hisham <ahmedhishamrasmy1@gmail.com>
2025-09-18 16:23:35 +0300

    chore: Comment out unused authenticate method in PaymobService

--- lib/core/utils/paymob_service.dart
+++ lib/core/utils/paymob_service.dart
@@ -7,22 +7,22 @@ import 'package:dio/dio.dart';
 class PaymobService {
   final ApiService apiService = ApiService();
 
-  Future<String?> authenticate() async {
-    final Dio dio = Dio();
-    const url = "https://accept.paymob.com/api/auth/tokens";
-    try {
-      final response = await dio.post(
-        url,
-        data: {"api_key": ApiKeys.apiKeyPaymob},
-      );
-      if (response.statusCode == 200 || response.statusCode == 201) {
-        final authToken = response.data['token'];
-        log("✅ Paymob Auth Token: $authToken");
-        return authToken;
-      }
-    } catch (e) {
-      log("❌ Paymob Authentication Error: $e");
-    }
-    return null;
-  }
+  // Future<String?> authenticate() async {
+  //   final Dio dio = Dio();
+  //   const url = "https://accept.paymob.com/api/auth/tokens";
+  //   try {
+  //     final response = await dio.post(
+  //       url,
+  //       data: {"api_key": ApiKeys.apiKeyPaymob},
+  //     );
+  //     if (response.statusCode == 200 || response.statusCode == 201) {
+  //       final authToken = response.data['token'];
+  //       log("✅ Paymob Auth Token: $authToken");
+  //       return authToken;
+  //     }
+  //   } catch (e) {
+  //     log("❌ Paymob Authentication Error: $e");
+  //   }
+  //   return null;
+  // }
 }
